e5e8b3c6fad02a192161d6db7b1f093982cfdca5
Ramón Berrutti <ramonberrutti@gmail.com>
2018-11-17 18:04:49 -0200

    Problem from 18 to 22

---
+++ 19-last-element.clj
@@ -0,0 +1,2 @@
+; (= ((fn [x] (nth x (- (count x) 1))) [1 2 3 4 5]) 5)
+(fn [x] (nth x (- (count x) 1)))
---
+++ 22-count-a-sequence.clj
@@ -0,0 +1,2 @@
+; (= (reduce (fn [x y] (inc x)) 0 '(1 2 3 3 1)) 5)
+reduce (fn [x y] (inc x)) 0
